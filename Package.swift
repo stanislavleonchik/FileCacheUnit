5dd4305dcb8037c0e05175df25e791a34e49576e
Stanislav Leonchik <leonstas1@mail.ru>
2024-07-19 06:07:32 +0300

    fix naming

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "FileCacheUnit",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15)
+        .macOS(.v10_15),
+        .iOS(.v13)
     ],
     products: [
         .library(
--- Sources/FileCacheUnit/FileCache.swift
+++ Sources/FileCacheUnit/FileCache.swift
@@ -1,27 +1,39 @@
 import Foundation
 
+public protocol JsonCompatible {
+    var json: Any { get }
+    static func parse(json: Any) -> Self?
+}
 
-public final class FileCache {
-    public var todoitems: [String: Todoitem] = [:]
-    public var items: [Todoitem] {
+public protocol CsvCompatible {
+    var csv: String { get }
+    static func parse(csv: Substring) -> Self?
+}
+
+@available(iOS 13.0, *)
+@available(macOS 10.15, *)
+public final class FileCache<T: Identifiable & JsonCompatible & CsvCompatible> {
+    public var todoitems: [T.ID: T] = [:]
+    public var revision: Int = 0
+    public var items: [T] {
         return Array(todoitems.values)
-    }
-
-    public func addItem(_ item: Todoitem) {
-        todoitems[item.id] = item
-    }
-    
-    public func removeItem(_ id: String) {
-        todoitems[id] = nil
-    }
-    
-    public func updateItem(_ id: String, _ item: Todoitem) {
-        todoitems[id] = item
     }
     
     public init() {}
+
+    public func addItem(_ item: T) {
+        todoitems[item.id] = item
+    }
     
-    public subscript(id: String) -> Todoitem? {
+    public func removeItem(_ id: T.ID) {
+        todoitems[id] = nil
+    }
+    
+    public func updateItem(_ id: T.ID, _ item: T) {
+        todoitems[id] = item
+    }
+    
+    public subscript(id: T.ID) -> T? {
         get {
             return todoitems[id]
         }
@@ -36,18 +48,19 @@ public final class FileCache {
 
         switch fileExtension {
         case "json":
-            let jsonData = try JSONSerialization.data(withJSONObject: todoitems.values.map { $0.json }, options: .prettyPrinted)
+            let json = todoitems.values.map { $0.json }
+            var jsonData = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+            jsonData.append(contentsOf: try JSONEncoder().encode(["revision": revision]))
             try jsonData.write(to: fileURL)
+            
         case "csv":
             var csvString = "id,text,importance,deadline,isDone,dateCreated,dateChanged\n"
-                    
             for item in items {
-                let deadline = item.deadline?.timeIntervalSince1970.description ?? ""
-                let dateChanged = item.dateChanged?.timeIntervalSince1970.description ?? ""
-                csvString.append("\(item.id),\"\(item.text)\",\(item.importance.rawValue),\(deadline),\(item.isDone),\(item.dateCreated.timeIntervalSince1970),\(dateChanged)\n")
+                csvString.append(item.csv)
             }
-
+            csvString.append("revision,\(revision)\n")
             try csvString.write(to: fileURL, atomically: true, encoding: .utf8)
+            
         default:
             throw NSError(domain: "FileCacheError", code: 1, userInfo: [NSLocalizedDescriptionKey: "Unsupported file format"])
         }
@@ -61,9 +74,13 @@ public final class FileCache {
         case "json":
             guard let jsonArray = try JSONSerialization.jsonObject(with: try Data(contentsOf: fileURL), options: []) as? [[String: Any]] else { return }
             for json in jsonArray {
-                let temp = Todoitem.parse(json: json)
-                if let id = temp?.id {
-                    todoitems[id] = temp
+                if let revisionValue = json["revision"] as? Int {
+                    revision = revisionValue
+                } else {
+                    let temp = T.parse(json: json)
+                    if let id = temp?.id {
+                        todoitems[id] = temp
+                    }
                 }
             }
             
@@ -71,15 +88,21 @@ public final class FileCache {
             let csvString = try String(contentsOf: fileURL)
             let rows = csvString.split(separator: "\n").dropFirst()
 
-            var loadedItems: [String: Todoitem] = [:]
+            var loadedItems: [T.ID: T] = [:]
             for row in rows {
-                guard let temp = Todoitem.parse(csv: row) else { continue }
-                loadedItems[temp.id] = temp
+                if row.hasPrefix("revision") {
+                    let revisionString = row.split(separator: ",").last ?? "0"
+                    revision = Int(revisionString) ?? 0
+                } else {
+                    guard let temp = T.parse(csv: row) else { continue }
+                    loadedItems[temp.id] = temp
+                }
             }
 
             for (id, item) in loadedItems {
                 todoitems[id] = item
             }
+        
         default:
             throw NSError(domain: "FileCacheError", code: 1, userInfo: [NSLocalizedDescriptionKey: "Unsupported file format"])
         }
